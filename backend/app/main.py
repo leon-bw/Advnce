39e37f5ec696c088bc0318dd2e89e2db317ef806
Leon Williams <leonbwilliams6@gmail.com>
2026-07-06 23:35:20 +0100

    Add health route and update main.py file

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.api.routes.auth import router as auth_router
+from app.api.routes.health import router as health_router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    description=settings.app_description,
+    version=settings.app_version,
+    debug=settings.app_debug,
+)
+
+
+app.include_router(health_router)
+app.include_router(auth_router, prefix=settings.API_V1_PREFIX)
